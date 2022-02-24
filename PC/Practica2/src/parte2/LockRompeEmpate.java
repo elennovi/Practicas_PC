26d1102ecb2ecc8181ad8a27867d32412affcc63
estizs <56733112+estizs@users.noreply.github.com>
2022-02-24 11:46:00 +0100

    Update LockRompeEmpate.java

--- PC/Practica2/src/parte2/LockRompeEmpate.java
+++ PC/Practica2/src/parte2/LockRompeEmpate.java
@@ -1,30 +1,32 @@
 package parte2;
-import java.util.concurrent.atomic.AtomicIntegerArray;
 
-public class LockRompeEmpate {
-	private AtomicIntegerArray in;
-	private AtomicIntegerArray last;
+public class LockRompeEmpate extends Lock{
+	private volatile int[] in;
+	private volatile int[] last;
 	private int N;
 	
 	LockRompeEmpate(int N) {
-		in = new AtomicIntegerArray(N);
-		last = new AtomicIntegerArray(N);
 		this.N = N;
+		in = new int[N + 1];
+		last = new int[N + 1];
 	}
 	
-	// El método que implementa el take
+	// El mï¿½todo que implementa el take
 	public void takeLock(int i) {
-		for (int j = 1; j < N; j++) {
-			in.set(i - 1, j);
-			last.set(j - 1, i);
+		for (int j = 1; j <= N; j++) {
+			in[i - 1] = j;
+			in = in;
+			last[j - 1] = i;
+			last = last;
 			for (int k = 1; k < N; k++)
 				if (k != i) 
-					while(in.get(k - 1) >= in.get(i - 1) && last.get(j - 1) == i);
+					while(in[k - 1] >= in[i - 1] && last[j - 1] == i);
 		}
 	}
 	
 	// El metodo que implementa el release
 	public void releaseLock(int i) {
-		in.set(i - 1, 0);
+		in[i - 1] = 0;
+		in = in;
 	}
 }
